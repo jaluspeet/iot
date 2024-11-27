1bf57a02ba17eb48c1fe6918d4dbda91a0d34b7c
AleBillo <alessandro.dominici2@studio.unibo.it>
2024-11-27 18:48:18 +0100

    Aggiunto Checkbox, Color Picking e altri parametri estetici

--- interface/script.js
+++ interface/script.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Loaded");
 });
+
+// Script per abilitare/disabilitare gli slider
+const lumosSlider = document.getElementById('lumos');
+const temperatureSlider = document.getElementById('temperature');
+const checkLumos = document.getElementById('checkLumos');
+const checkTemperature = document.getElementById('checkTemperature');
+
+// Funzione per resettare il valore dello slider e disabilitarlo
+const toggleSlider = (checkbox, slider) => {
+    if (checkbox.checked) {
+        slider.disabled = false;
+    } else {
+        slider.disabled = true;
+        slider.value = 50; // Valore di default centrale
+    }
+};
+
+// Event listener per Luminosità
+checkLumos.addEventListener('change', () => toggleSlider(checkLumos, lumosSlider));
+
+// Event listener per Temperatura
+checkTemperature.addEventListener('change', () => toggleSlider(checkTemperature, temperatureSlider));
